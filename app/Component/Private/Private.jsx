dc6a8e81dae52529853be3b2e0c19c861a0dc253
Tasnim813 <tasnim3j@gmail.com>
2025-11-26 18:56:14 +0600

    private

--- app/Component/Private/Private.jsx
+++ app/Component/Private/Private.jsx
@@ -1,18 +1,29 @@
+'use client'
 import useAuth from '@/app/hook/useAuth';
-import { useRouter } from 'next/router';
-import React from 'react';
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react';
 import Loader from '../Loader/Loader';
 
 const Private = ({children}) => {
-    const {user,loading}=useAuth()
-    const router=useRouter()
-    if(loading){
-        return <Loader></Loader>
+    const { user, loading } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!loading && !user) {
+            router.push("/login");
+        }
+    }, [loading, user, router]);
+
+    if (loading) {
+        return <Loader />;
     }
-    if(!user){
-        return router.push('/login')
+
+    if (!user) {
+        return null; // redirect hocche, tai empty return
     }
+
     return children;
 };
 
 export default Private;
+
